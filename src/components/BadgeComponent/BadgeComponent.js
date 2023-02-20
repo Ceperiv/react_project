e970f1b15ef6e649f7e68c84e0111f7ebfe59afb
Andriy Konoval <ceperiv@dmail.com>
2023-02-20 17:34:53 +0200

    console.logs deleted

--- src/components/BadgeComponent/BadgeComponent.js
+++ src/components/BadgeComponent/BadgeComponent.js
@@ -14,10 +14,11 @@ function BadgeComponent({movie}) {
     const date = new Date(+year, month - 1, +day, +'00', +'00', +'00');
     const timeStampInSeconds = date.getTime();
     const currentDate = new Date().getTime()
+    const days_41 = 3542400000
 
     return (
         <div className={'BadgeComponent'}>
-            {timeStampInSeconds > (currentDate - 3600000000) && <Badge className={'badge'} content={'NEW'}/>}
+            {timeStampInSeconds > (currentDate - days_41) && <Badge className={'badge'} content={'NEW'}/>}
         </div>
     );
 }
